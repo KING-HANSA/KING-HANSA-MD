dd0e0e2f6f2bd52b39f0eccfe841aca29851b562
KING-HANSA <hansadewmina2008@gmail.com>
2024-09-06 19:04:49 +0530

    𝕂𝕀ℕ𝔾 ℍ𝔸ℕ𝕊𝔸

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94701571591']
+const ownerNumber = ['94701515609']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
